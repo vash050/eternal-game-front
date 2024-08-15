974e2895194f5073b11b8d5da2edf6b1a5912273
vasiliy <vash050@rambler.ru>
2024-08-15 19:21:16 +0300

    add pixi

--- eternal-game/src/views/GameMap.vue
+++ eternal-game/src/views/GameMap.vue
@@ -1,12 +1,11 @@
 <script setup>
-
-
-
-
-
-
+import { Application } from "vue3-pixi";
 </script>
 
 <template>
-<div>Map</div>
+  <Application :width="240" :height="240">
+    <text :anchor="0.5" :x="120" :y="120" :style="{ fill: 'white' }">
+      Hello World
+    </text>
+  </Application>
 </template>
