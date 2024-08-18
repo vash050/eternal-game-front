adf7c1e98f61913f644afa6c2671ee8c38a9f779
vasiliy <vash050@rambler.ru>
2024-08-18 16:05:05 +0300

    add player move

--- eternal-game/src/views/GameMap.vue
+++ eternal-game/src/views/GameMap.vue
@@ -1,5 +1,7 @@
 <script>
+import {useEventListener} from "@vueuse/core";
 import * as PIXI from 'pixi.js';
+
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
@@ -39,6 +41,56 @@ export default {
       player.x = this.playerPosX;
       player.y = this.playerPosY;
       app.stage.addChild(player);
+
+      
+
+      //let elapsed = 0.0;
+      //app.ticker.add((ticker) => {
+       // elapsed += ticker.deltaTime;
+       // player.x = 100.0 + Math.cos(elapsed/150.0) * 100.0;
+      //});
+
+      app.stage.addEventListener('click', (e) => {
+        console.log(e.global)
+        // mapMain.position.x = e.global.x;
+        // mapMain.position.y = e.global.y;
+      });
+
+
+
+
+      useEventListener('keydown', (e) => {
+        switch (e.key) {
+          case 'w':
+            if (sprite.position.y >= player.position.y) {
+              break;
+            }
+            sprite.position.y += 5;
+            break;
+          case 's':
+            if (sprite.position.y <= (player.position.y - sprite.height + player.height)){
+              break;
+            }
+            sprite.position.y -= 5;
+            break;
+          case 'a':
+            if (sprite.position.x >= player.position.x) {
+              break;
+            }
+            if (sprite.position.x >= player.position.x) {
+              break;
+            }
+            sprite.position.x += 5;
+            break;
+          case 'd':
+            if (sprite.position.x <= (player.position.x - sprite.width + player.width)) {
+              break;
+            }
+            sprite.position.x -= 5;
+            break;
+        }
+      });
+
 
     },
   },
