413ee6fa039c5a7dd58ec70fb7ae811866396a77
vasiliy <vash050@rambler.ru>
2024-08-21 15:51:08 +0300

    create template almin

--- eternal-game/src/views/AdminView.vue
+++ eternal-game/src/views/AdminView.vue
@@ -1,6 +1,39 @@
+<script>
+export default {
+  setup() {},
+  data() {
+    return {
+      listParamers: [
+        {name: "human", direction: "sssssssssssssssss"},
+        {},
+      ],
+    };
+  },
+};
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="grid grid-cols-3 gap-4">
+    <div class="col-span-3 justify-items-center">
+      <h1 class="flex justify-center">Админка</h1>
+    </div>
+    <ul class="col-span-3 grid grid-cols-3 gap-4">
+      <li class="col-span-3 flex justify-between">
+        <div>название</div>
+        <div>описание</div>
+        <div>редактировать</div>
+      </li>
+      <hr>
+      <li
+        v-for="el in listParamers"
+        class="col-span-3 flex justify-between"
+      >
+        <div>{{ el.name }}</div>
+        <div>{{ el.direction }}</div>
+        <div><button>редактировать</button></div>
+      </li>
+
+    </ul>
   </div>
 </template>
 
