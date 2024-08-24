4d2f501ceba07092dc75906e46cbed22fc5eb1e5
vasiliy <vash050@rambler.ru>
2024-08-24 22:03:37 +0300

    -fix загрузку характиристик

--- eternal-game/src/views/AdminPageDataView.vue
+++ eternal-game/src/views/AdminPageDataView.vue
@@ -6,21 +6,22 @@ export default {
   name: 'AdminPageData',
   data() {
     return {
-      el: '',
+      el: 'units/race/races',
       all_data: [],
     };
   },
 
-  // TODO: исправить метод, чтобы отрисовывал не только при создании страницы
+  // TODO: исправить первую загрузку
   created() {
-    const skill_el = this.$route.params.el;
-    if (skill_el) {
-      this.el = skill_el;
-    }
+    this.$watch(
+        () => this.$route.params.el,
+        (newEl, oldEl) => {
+          this.all_data = [];
+          this.el = newEl;
+          this.getAllDate();
+        }
+    );
 
-  },
-  mounted() {
-    this.getAllDate();
   },
   methods: {
     async getAllDate() {
@@ -47,27 +48,27 @@ export default {
 </script>
 
 <template>
-	<div class="container">
-		<h1>Административная страница</h1>
-		<hr>
-		<div class="">
-			<div class="" v-for="item in all_data" :key="item.id">
-				<div class="flex grid-rows-6 justify-between">
-					<p class="text-sm">{{ item.name }}</p>
-					<p class="text-sm">{{ item.description }}</p>
+  <div class="container">
+    <h1>Административная страница</h1>
+    <hr>
+    <div class="">
+      <div class="" v-for="item in all_data" :key="item.id">
+        <div class="flex grid-rows-6 justify-between">
+          <p class="text-sm">{{ item.name }}</p>
+          <p class="text-sm">{{ item.description }}</p>
 
-					<button class="bg-indigo-500 text-xs border-2 border-black rounded-full">
-						Редактировать
-					</button>
-					<button class="bg-red-500 text-xs border-2  rounded-full border-black">
-						Удалить
-					</button>
-				</div>
-				<hr/>
-			</div>
-		</div>
-		<button class="bg-indigo-500 text-xs border-2 border-black  rounded-full">
-			Создать
-		</button>
-	</div>
+          <button class="bg-indigo-500 text-xs border-2 border-black rounded-full">
+            Редактировать
+          </button>
+          <button class="bg-red-500 text-xs border-2  rounded-full border-black">
+            Удалить
+          </button>
+        </div>
+        <hr/>
+      </div>
+    </div>
+    <button class="bg-indigo-500 text-xs border-2 border-black  rounded-full">
+      Создать
+    </button>
+  </div>
 </template>
